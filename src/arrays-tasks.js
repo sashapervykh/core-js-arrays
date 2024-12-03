a8787d52991d8ca5e4a9a1aab216d3aad21c5951
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-12-03 15:10:35 +0300

    feat: add sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -40,8 +40,20 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  return arr1.length > arr2.length
+    ? arr1.map((elem, index) => {
+        if (arr2[index]) {
+          return elem + arr2[index];
+        }
+        return elem;
+      })
+    : arr2.map((elem, index) => {
+        if (arr1[index]) {
+          return elem + arr1[index];
+        }
+        return elem;
+      });
 }
 
 /**
