332c25e333820e2fc62de2e7fa0b5ab049520b30
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-12-04 19:43:57 +0300

    feat: add calculateBalance

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -329,8 +329,8 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce((acc, curr) => acc + curr[0] - curr[1], 0);
 }
 
 /**
