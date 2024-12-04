cb872f0ba250cda035f5b6732a07c18588e93923
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-12-04 20:00:17 +0300

    feat: add createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -345,8 +345,14 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return Array.from({ length: Math.ceil(arr.length / chunkSize) }).map(
+    (elem, index) => {
+      return Array.from(
+        arr.slice(index * chunkSize, index * chunkSize + chunkSize)
+      );
+    }
+  );
 }
 
 /**
