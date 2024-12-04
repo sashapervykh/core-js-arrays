3cdc2867370357fd85fa37d170c8962c874ef6c5
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-12-04 20:09:27 +0300

    feat: add generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -367,8 +367,9 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = new Array(len).fill(1);
+  return arr.map((elem, index) => index * 2 + elem);
 }
 
 /**
