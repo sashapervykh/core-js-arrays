7387328812c33296da9f70d26541f358fe413f29
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-12-06 12:26:36 +0300

    feat: getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -442,8 +442,13 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .map((elem, index) => {
+      if (elem % 2 !== 0) return index;
+      return undefined;
+    })
+    .filter((elem) => elem !== undefined);
 }
 
 /**
