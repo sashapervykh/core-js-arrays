7a59f8361a1cc44f4576c44a68c9d7377ea71df0
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-12-07 12:06:59 +0300

    feat: add shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -547,8 +547,11 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const step = n < 0 ? Math.abs(n) - 1 : Math.abs(n);
+  return arr
+    .filter((elem, index) => step < index)
+    .concat(arr.filter((elem, index) => index <= step));
 }
 
 /**
